0b3597a0a83176181a899266cb0f264cf7fcda2b
Joel Pettersson <petterssonjoel@gmail.com>
2014-03-30 01:31:36 +0100

    Show all tournaments and default to current period

--- app/src/main/java/com/ngusta/cupassist/activity/TournamentListActivity.java
+++ app/src/main/java/com/ngusta/cupassist/activity/TournamentListActivity.java
@@ -2,16 +2,21 @@ package com.ngusta.cupassist.activity;
 
 import com.ngusta.cupassist.R;
 import com.ngusta.cupassist.adapters.TournamentListAdapter;
+import com.ngusta.cupassist.domain.CompetitionPeriod;
 import com.ngusta.cupassist.domain.Tournament;
 import com.ngusta.cupassist.service.TournamentService;
 
 import android.app.ListActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SectionIndexer;
 
+import java.util.Date;
 import java.util.List;
 
 public class TournamentListActivity extends ListActivity {
@@ -33,12 +38,43 @@ public class TournamentListActivity extends ListActivity {
         TournamentActivity.startActivity(this, item);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(Menu.NONE, R.id.menu_item_today, Menu.NONE, R.string.current_competition_period)
+                .setIcon(android.R.drawable.ic_menu_today)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.menu_item_today) {
+            selectCurrentCompetitionPeriod();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void setListAdapter(ListAdapter adapter) {
+        super.setListAdapter(adapter);
+        selectCurrentCompetitionPeriod();
+    }
+
+    private void selectCurrentCompetitionPeriod() {
+        SectionIndexer listAdapter = (SectionIndexer) getListAdapter();
+        CompetitionPeriod period = CompetitionPeriod.findPeriodByDate(new Date());
+        int position = listAdapter.getPositionForSection(period.getPeriodNumber() - 1);
+        setSelection(position);
+    }
+
     private class RequestTournamentsTask extends AsyncTask<Void, String, List<Tournament>> {
 
         @Override
         protected List<Tournament> doInBackground(Void... voids) {
-            return new TournamentService(TournamentListActivity.this)
-                    .getTournamentsFromCurrentCompetitionPeriodAndLater();
+            return new TournamentService(TournamentListActivity.this).getAllTournaments();
         }
 
         @Override
