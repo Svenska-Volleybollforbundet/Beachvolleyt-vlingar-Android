b965bb94b9fc05b047139ec269852eabf5babd45
ngusta <ngusta1@gmail.com>
2016-03-22 13:50:31 +0100

    Set green background for teams that have paid

--- app/src/main/java/com/ngusta/cupassist/adapters/TeamAdapter.java
+++ app/src/main/java/com/ngusta/cupassist/adapters/TeamAdapter.java
@@ -5,10 +5,12 @@ import com.ngusta.cupassist.domain.Tournament;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -38,6 +40,7 @@ public class TeamAdapter extends ArrayAdapter<Tournament.TeamGroupPosition> {
             row = inflater.inflate(resource, parent, false);
 
             holder = new TeamHolder();
+            holder.team = (RelativeLayout) row.findViewById(R.id.team);
             holder.group = (TextView) row.findViewById(R.id.group);
             holder.playerAName = (TextView) row.findViewById(R.id.playerA_name);
             holder.playerBName = (TextView) row.findViewById(R.id.playerB_name);
@@ -49,6 +52,11 @@ public class TeamAdapter extends ArrayAdapter<Tournament.TeamGroupPosition> {
         }
 
         Tournament.TeamGroupPosition team = teams.get(position);
+        if (team.team.hasPaid()) {
+            holder.team.setBackgroundColor(0x2000c800);
+        } else {
+            holder.team.setBackgroundColor(Color.WHITE);
+        }
         if (position < maxNumberOfTeams) {
             holder.group.setText("" + team.group);
         } else {
@@ -61,6 +69,8 @@ public class TeamAdapter extends ArrayAdapter<Tournament.TeamGroupPosition> {
     }
 
     static class TeamHolder {
+
+        RelativeLayout team;
 
         TextView group;
 
