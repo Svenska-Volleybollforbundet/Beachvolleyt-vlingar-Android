ea228811f718f843a2c91ae5c65dfff5d36816d0
Niklas Gustafsson <ngusta1@gmail.com>
2016-04-16 13:14:27 +0200

    Added firebase authentication.

--- backend/src/main/java/com/ngusta/beachvolley/backend/FirebaseUpdateServlet.java
+++ backend/src/main/java/com/ngusta/beachvolley/backend/FirebaseUpdateServlet.java
@@ -1,6 +1,6 @@
 package com.ngusta.beachvolley.backend;
 
-
+import com.firebase.client.AuthData;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
@@ -19,29 +19,34 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class MyServlet extends HttpServlet {
+public class FirebaseUpdateServlet extends HttpServlet {
 
-    public static final String ALL = "all";
+    private static final String ALL = "all";
 
-    public static final String ACTIVE_TOURNAMENTS = "active";
+    private static final String ACTIVE_TOURNAMENTS = "active";
 
-    public static final int TWO_WEEKS_IN_MS = 14 * 24 * 3600 * 1000;
+    private static final int ONE_DAY_IN_MS = 24 * 3600 * 1000;
+
+    private static final int TWO_WEEKS_IN_MS = 14 * 24 * 3600 * 1000;
+
+    private static final String FIREBASE_SECRET = "nzgY8tDeQQKWgGvBCt98EGoQz1zNVp7HA0s2Fkvg";
 
     private Firebase firebase;
-
     private Map<String, Player> players;
-
     private List<Tournament> tournaments;
+
+    private Logger log = Logger.getLogger("FirebaseUpdateServlet");
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
-        firebase = new Firebase("https://beachvolleydb.firebaseio.com/v1");
+        getFirebase();
         long start = System.currentTimeMillis();
         String path = req.getPathInfo().substring(1);
         firebase.child("Latest update " + path + " run").setValue(new Date().toString());
@@ -54,8 +59,23 @@ public class MyServlet extends HttpServlet {
                 break;
         }
         long duration = System.currentTimeMillis() - start;
-        System.out.println("Run time for " + path + ": " + duration + " ms");
+        log.info("Run time for " + path + ": " + duration + " ms");
         firebase.child("Run time for " + path).setValue(duration + " ms");
+    }
+
+    private void getFirebase() {
+        firebase = new Firebase("https://beachvolleydb.firebaseio.com/v1");
+        firebase.authWithCustomToken(FIREBASE_SECRET, new Firebase.AuthResultHandler() {
+            @Override
+            public void onAuthenticated(AuthData authData) {
+                log.info("Firebase authenticated.");
+            }
+
+            @Override
+            public void onAuthenticationError(FirebaseError firebaseError) {
+                log.info("Authentication error when connection to Firebase: " + firebaseError.getMessage());
+            }
+        });
     }
 
     private void updateAll() throws IOException {
@@ -66,7 +86,7 @@ public class MyServlet extends HttpServlet {
         Map<String, Player> players = playerListCache.getPlayers();
         firebase.child("players").setValue(players);
 
-        System.out.println("Updated all " + players.size() + " players.");
+        log.info("Updated all " + players.size() + " players.");
         for (Tournament tournament : tournaments.values()) {
             if (tournament.getUrl() != null && !tournament.getUrl().isEmpty()) {
                 List<Team> teams = tournamentListCache.getTournamentDetails(tournament, players);
@@ -74,32 +94,28 @@ public class MyServlet extends HttpServlet {
             }
         }
         firebase.child("tournaments").setValue(tournaments);
-        System.out.println("Updated all " + tournaments.size() + " tournaments.");
+        log.info("Updated all " + tournaments.size() + " tournaments.");
     }
 
     private void updateActive() throws IOException {
         requestPlayers();
-        waitForPlayers();
-        System.out.println("Players read");
-        TournamentListCache tournamentListCache = new TournamentListCache();
-        long now = System.currentTimeMillis();
-        long twoWeeksInTheFuture = System.currentTimeMillis() + TWO_WEEKS_IN_MS;
-        Query qref = firebase.child("tournaments").orderByChild("startDate").startAt(now).endAt(twoWeeksInTheFuture);
-        requestActiveTournaments(qref);
-        waitForTournaments();
+        requestActiveTournaments();
+        waitForTournamentsAndPlayers();
+        log.info("Players and tournaments read");
 
+        TournamentListCache tournamentListCache = new TournamentListCache();
         setCookie();
         for (Tournament tournament : tournaments) {
             try {
                 if (tournament.getUrl() != null && !tournament.getUrl().isEmpty()) {
                     List<Team> teams = tournamentListCache.getTeams(tournament, players);
                     firebase.child("registeredTeams/" + tournament.uniqueIdentifier()).setValue(teams);
-                    System.out.println("Updated " + teams.size() + " teams for " + tournament.getName());
+                    log.info("Updated " + teams.size() + " teams for " + tournament.getName());
                 } else {
-                    System.out.println("Tried to update the tournament " + tournament.getName() + ", but it wasn't open.");
+                    log.info("Tried to update the tournament " + tournament.getName() + ", but it wasn't open.");
                 }
             } catch (IOException exception) {
-                System.out.println("Could not parse teams for " + tournament.toString());
+                log.info("Could not parse teams for " + tournament.toString());
             }
         }
     }
@@ -121,37 +137,29 @@ public class MyServlet extends HttpServlet {
         });
     }
 
-    private void waitForPlayers() {
-        try {
-            while (players == null || players.isEmpty()) {
-                Thread.sleep(100);
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void requestActiveTournaments(Query qref) {
-        final List<Tournament> downloadedTournaments = new ArrayList<>();
+    private void requestActiveTournaments() {
+        long now = System.currentTimeMillis() - ONE_DAY_IN_MS;
+        long twoWeeksInTheFuture = System.currentTimeMillis() + TWO_WEEKS_IN_MS;
+        Query qref = firebase.child("tournaments").orderByChild("startDate").startAt(now).endAt(twoWeeksInTheFuture);
+        final List<Tournament> activeTournaments = new ArrayList<>();
         qref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    downloadedTournaments.add(snapshot.getValue(Tournament.class));
+                    activeTournaments.add(snapshot.getValue(Tournament.class));
                 }
-                tournaments = downloadedTournaments;
+                tournaments = activeTournaments;
             }
 
             @Override
             public void onCancelled(FirebaseError firebaseError) {
-
             }
         });
     }
 
-    private void waitForTournaments() {
+    private void waitForTournamentsAndPlayers() {
         try {
-            while (tournaments == null || tournaments.isEmpty()) {
+            while (tournaments == null || players == null) {
                 Thread.sleep(100);
             }
         } catch (InterruptedException e) {
