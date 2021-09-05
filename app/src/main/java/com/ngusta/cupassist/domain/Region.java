483b7d54f28bd3df2e8e29f3e10862c334a30c0b
ngusta <ngusta1@gmail.com>
2021-09-05 21:08:26 +0200

    Added new clubs to region fitler

--- app/src/main/java/com/ngusta/cupassist/domain/Region.java
+++ app/src/main/java/com/ngusta/cupassist/domain/Region.java
@@ -5,14 +5,14 @@ import java.util.List;
 import static java.util.Arrays.asList;
 
 public enum Region {
-    NORTH(asList("Back to the beach", "IK Studenterna i Umeå", "Lule Volley", "Härnösands VBK")),
-    EAST(asList("Back to the beach", "08 Beachvolley", "Bromma KFUK-KFUM", "Fyrishov BC", "Södertelge VBK", "Stockholm BVC", "Linköping Beach AC",
-            "Team Gotland Volleybollklubb")),
-    WEST(asList("Back to the beach", "Beachbrothers BC", "Kalmar VBK", "Mariestads VBK", "Morgondagens BC", "Göteborg BC", "Habo WK 87",
-            "Karlstads VK")),
-    SOUTH(asList("Back to the beach", "Jönköpings BC", "Föreningen Beachvolstey-Aid", "Växjö VK", "Ljungby VBK", "KFUM Gymnastik & IA Karskrona",
-            "Malmö BC",
-            "Lunds VK", "Svedala VBK", "IFK Helsingborg", "Falkenbergs VBK", "KFUM Kristianstad Volleybollklubb"));
+    NORTH(asList("Back to the beach", "IK Studenterna i Umeå", "Lule Volley", "Härnösands VBK", "Stöcke IF", "IFK Söderhamn")),
+    EAST(asList("Back to the beach", "08 Beachvolley", "08 Beachvolley Club", "Bromma KFUK-KFUM", "Fyrishov BC", "Södertelge VBK", "Stockholm BVC", "Linköping Beach AC",
+            "Team Gotland Volleybollklubb", "Sollentuna VK", "Västerås VBK", "Team Gotland", "Nyköpings FK", "Sigtuna BVC")),
+    WEST(asList("Back to the beach", "08 Beachvolley Club", "Beachbrothers BC", "Kalmar VBK", "Mariestads VBK", "Morgondagens BC", "Göteborg BC", "Habo WK 87",
+            "Karlstads VK", "Habo Wolley", "Västra Ämterviks IF", "Falköpings VK", "Trollhättans VK", "Skara Volleybollklubb", "IK Ymer", "Mölltorp Beachvolley", "SVBF")),
+    SOUTH(asList("Back to the beach", "Jönköpings BC", "Föreningen Beachvolstey-Aid", "Beachvolley-Aid", "Växjö VK", "Ljungby VBK", "KFUM Gymnastik & IA Karskrona",
+            "Karlskrona Volleyboll", "Malmö BC", "Slow Down",
+            "Lunds VK", "Svedala VBK", "IFK Helsingborg", "Falkenbergs VBK", "KFUM Kristianstad Volleybollklubb", "KFUM Kristianstad"));
 
     private List<String> clubs;
 
@@ -33,6 +33,7 @@ public enum Region {
         if (SOUTH.clubs.contains(club)) {
             return SOUTH;
         }
+        System.err.println("Club " + club + " is not part of a region.");
         return null;
     }
 }
